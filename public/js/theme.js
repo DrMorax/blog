3133d04ee3c10946fae5a19b2279bdd0a38c5fdc
DrMorax <abbasbusiness01@gmail.com>
2025-10-23 17:57:44 +0300

    palette controls

--- public/js/theme.js
+++ public/js/theme.js
@@ -4,24 +4,47 @@ const root = document.documentElement;
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme === "light" || savedTheme === "dark") {
-    root.classList.add(savedTheme);
+  root.classList.add(savedTheme);
 } else {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        root.classList.add("dark");
-    } else {
-        root.classList.add("light");
-    }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    root.classList.add("dark");
+  } else {
+    root.classList.add("light");
+  }
 }
 
-root.classList.add("anemo");
-
 document.getElementById("themeToggler")?.addEventListener("click", () => {
-    if (root.classList.contains("light")) {
-        root.classList.replace("light", "dark");
-        localStorage.setItem("theme", "dark");
-    } else {
-        root.classList.replace("dark", "light");
-        localStorage.setItem("theme", "light");
-    }
+  if (root.classList.contains("light")) {
+    root.classList.replace("light", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    root.classList.replace("dark", "light");
+    localStorage.setItem("theme", "light");
+  }
+});
+
+const savedPalette = localStorage.getItem("palette");
+if (savedPalette) root.classList.add(savedPalette);
+
+const paletteItems = document.querySelectorAll(".palettes li");
+const paletteClasses = [
+  "anemo",
+  "geo",
+  "electro",
+  "dendro",
+  "hydro",
+  "pyro",
+  "cryo",
+];
+
+paletteItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const paletteValue = item.innerHTML.toLowerCase().trim();
+    paletteClasses.forEach((paletteClass) => {
+      root.classList.remove(paletteClass);
+    });
+    root.classList.add(paletteValue);
+    localStorage.setItem("palette", paletteValue);
+  });
 });
 // --- End ---
