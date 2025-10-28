86fc14cbce65387524e758ea83d813b2c127d599
DrMorax <abbasbusiness01@gmail.com>
2025-10-28 22:29:45 +0300

    feat: home feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import icon from 'astro-icon';
-import path from 'path';
-
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://morax.dev",
-    integrations: [sitemap(), icon()],
-    server: {
-        port: 3000,
+  site: "https://morax.dev",
+  integrations: [sitemap(), icon()],
+  server: {
+    port: 3000,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
     },
-    vite: {
-        resolve: {
-            alias: {
-                '@': path.resolve('./src'), 
-            },
-        },
-    },
+  },
 });
