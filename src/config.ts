d688978d5bb43f3344697a2cad010ef6cbb2c1b0
DrMorax <abbasbusiness01@gmail.com>
2026-02-01 02:16:49 +0300

    about page

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,15 @@
 type Links = {
-    nav: Navlink[];
+  nav: Navlink[];
 };
 
 type Navlink = {
-    name: string;
-    href: string;
+  name: string;
+  href: string;
 };
 
 export const links: Links = {
-    nav: [
-        { name: "posts", href: "/posts" },
-        { name: "about", href: "/about" },
-        { name: "explore", href: "/explore" },
-    ],
+  nav: [
+    { name: "posts", href: "/posts" },
+    { name: "about", href: "/about" },
+  ],
 };
