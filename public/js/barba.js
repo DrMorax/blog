83bc3e978a99ec3b9bade8bd7f47b4a1928d9e93
DrMorax <abbasbusiness01@gmail.com>
2026-03-04 06:54:00 +0300

    cooler animations

--- public/js/barba.js
+++ public/js/barba.js
@@ -2,54 +2,16 @@ import barba from "/js/barba.mjs";
 
 // View transition stuff
 // --- Start ---
-barba.init({
-  transitions: [
-    {
-      name: "fade-vertical-fast",
-      leave({ current }) {
-        // Apply fade + down transform
-        current.container.style.transition =
-          "opacity 0.3s ease, transform 0.3s ease";
-        current.container.style.opacity = 0;
-        current.container.style.transform = "translateY(20px)";
-
-        // Return a promise that resolves after the transition ends
-        return new Promise((resolve) => {
-          current.container.addEventListener(
-            "transitionend",
-            function handler(e) {
-              if (e.propertyName === "opacity") {
-                current.container.removeEventListener("transitionend", handler);
-                resolve();
-              }
-            },
-          );
-        });
-      },
-      enter({ next }) {
-        // Start below
-        next.container.style.opacity = 0;
-        next.container.style.transform = "translateY(20px)";
-
-        // Force reflow to trigger transition
-        next.container.getBoundingClientRect();
-
-        // Animate up to normal
-        next.container.style.transition =
-          "opacity 0.3s ease, transform 0.3s ease";
-        next.container.style.opacity = 1;
-        next.container.style.transform = "translateY(0)";
-      },
-    },
-  ],
-});
+barba.init();
 
 let transitioning = false;
+
 barba.hooks.before(() => {
   if (transitioning) return;
   window.scrollTo(0, 0);
   transitioning = true;
 });
+
 barba.hooks.after(() => {
   transitioning = false;
 
@@ -59,5 +21,9 @@ barba.hooks.after(() => {
     const top = window.scrollY + img.getBoundingClientRect().top - offset;
     window.scrollTo({ top, behavior: "smooth" });
   }
+
+  document.querySelectorAll(".onload-animation").forEach((section, index) => {
+    section.style.animationDelay = `calc(${index + 1} * var(--onload-animation-interval))`;
+  });
 });
 // --- End ---
