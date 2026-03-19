eab821c300fa1504ea10b74152d05fa7d46ebf35
DrMorax <abbasbusiness01@gmail.com>
2026-03-19 21:58:53 +0300

    New guestbook page

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -7,9 +7,26 @@ type Navlink = {
   href: string;
 };
 
+export type Guestbook = {
+  success: boolean;
+  data: GuestbookMessage[];
+};
+
+export type GuestbookMessage = {
+  id: string;
+  name: string;
+  message: string;
+  created_at: Date;
+};
+
 export const links: Links = {
   nav: [
     { name: "posts", href: "/posts" },
     { name: "about", href: "/about" },
+    { name: "guest", href: "/guest" },
   ],
 };
+
+export const API_URL = import.meta.env.PROD
+  ? "https://api.morax.dev"
+  : "http://localhost:4000";
