0f3c534edcfa904823f3a08bb18ccc945485e89e
DrMorax <abbasbusiness01@gmail.com>
2026-07-06 14:51:06 +0300

    more subtle animations

--- ui/public/js/barba.js
+++ ui/public/js/barba.js
@@ -11,12 +11,12 @@ barba.hooks.before(() => {
 });
 
 barba.hooks.after(() => {
-  const img = document.querySelector("article img");
-  if (img) {
-    const offset = 300;
-    const top = window.scrollY + img.getBoundingClientRect().top - offset;
-    window.scrollTo({ top, behavior: "smooth" });
-  }
+  // const img = document.querySelector("article img");
+  // if (img) {
+  //   const offset = 300;
+  //   const top = window.scrollY + img.getBoundingClientRect().top - offset;
+  //   window.scrollTo({ top, behavior: "smooth" });
+  // }
 
   document.querySelectorAll(".onload-animation").forEach((section, index) => {
     section.style.animationDelay = `calc(${index + 1} * var(--onload-animation-interval))`;
